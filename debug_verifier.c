cad8cba1d18b94674a4cc1d47df3c1d98215fd75
mr-r3bot <vothanhtruongquang@gmail.com>
2021-11-03 11:40:43 -0400

    add headers and convert type to uint64

--- debug_verifier.c
+++ debug_verifier.c
@@ -2,7 +2,9 @@
 #include <linux/perf_event.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <stdlib.h>
 #include <sys/stat.h>
+#include <linux/version.h>
 #include <linux/hw_breakpoint.h>
 #include <sys/syscall.h>
 #include <fcntl.h>
@@ -15,6 +17,7 @@ int main(int argc, char **argv) {
     char verifier_log_output[0x200000] = {0};
     unsigned char buf[1024] = {};
     int bfd;
+    int n;
     struct bpf_insn *insn;
 
     bfd = open("bin/trace_kern.o", O_RDONLY);
@@ -24,22 +27,23 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    int n = read(bfd, buf, sizeof(buf));
+    n = read(bfd, buf, sizeof(buf));
     close(bfd);
     insn = (struct bpf_insn*)buf;
     union bpf_attr prog_attr = {
         .prog_type = BPF_PROG_TYPE_TRACEPOINT,
         .insns = (uint64_t)insn,
+        .insn_cnt = n / sizeof(struct bpf_insn),
         .license = (uint64_t)"",
         .log_level = 2,
         .log_size = sizeof(verifier_log_output),
-        .log_buf = verifier_log_output
+        .log_buf = (uint64_t)verifier_log_output
     };
 
     int pdf = syscall(SYS_bpf, BPF_PROG_LOAD, &prog_attr, sizeof(prog_attr));
     if (pdf < 0 ) {
         fprintf(stderr, "Unable to load program:\n");
-        printf("Log buf = %s\n", verifier_log_output);
+        printf("Log buf:\n%s\n", verifier_log_output);
         exit(-1);
     }
 }
